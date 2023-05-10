b621397daa6f02539d09b44c1bd7a3481a5861e9
Luís Fernando <luisfernandodepaulosousa0020@gmail.com>
2023-05-10 15:29:02 -0300

    Projeto finalizado

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,16 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './TodoList.css'
 import Icone from'./assets/public/icon.webp';
 
 function TodoList() {
-  const[lista, setLista] = useState([])
+  const listaStorage = localStorage.getItem('Lista')
+
+  const[lista, setLista] = useState(listaStorage ? JSON.parse(listaStorage) : [])
   const[novoItem, setNovoItem] = useState('')
+
+  useEffect(()=> {
+    localStorage.setItem('Lista', JSON.stringify(lista));
+  },[lista])
 
   function adicionaItem(form) {
     form.preventDefault()
@@ -14,6 +20,22 @@ function TodoList() {
     setLista([...lista, {text: novoItem, isCompleted: false}])
     setNovoItem('')
     document.getElementById('input-entrada').focus()
+  }
+
+  function clicou(index) {
+    const listaAux = [...lista]
+    listaAux[index].isCompleted = !listaAux[index].isCompleted
+    setLista(listaAux)
+  }
+
+  function deleta(index) {
+    const listaAux = [...lista]
+    listaAux.splice(index, 1)
+    setLista(listaAux)
+  }
+
+  function deletaTudo() {
+    setLista([])
   }
 
   return (
@@ -30,23 +52,24 @@ function TodoList() {
         <button className='add' type="submit">Add</button>
       </form> 
       <div className="listaTarefas">
+        <div style={{textAlign:'center'}}>
         {
           lista.length < 1
           ?
-          <img src={Icone}/>
+          <img className="icone-central" src={Icone}/>
           :
           lista.map((item, index)=>(
-            <div className="item">
-              <span>{item.text}</span>
-              <button className="del">Deletar</button>
+            <div key={index} className={item.isCompleted ? 'item completo' : 'item'}>
+              <span onClick={()=>{clicou(index)}}>{item.text}</span>
+              <button onClick={()=>{deleta(index)}} className="del">Deletar</button>
             </div>
           ))
         } 
-        <div className="item completo">
-          <span>Tarefa de exemplo</span>
-          <button className="del">Deletar</button>
+        {
+          lista.length > 0 && 
+          <button onClick={()=>{deletaTudo()}} className="deletAll">Deletar Todas</button>
+        }
         </div>
-        <button className="deletAll">Deletar Todas</button>
       </div>
     </div>
   )
