e2a53fd0bb1b020dcbc9cbac18f6eec9d17bee3e
Luís Fernando <luisfernandodepaulosousa0020@gmail.com>
2023-05-10 14:01:58 -0300

    Formatado com css o titulo e o input de texto

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import './TodoList.css'
 
 function TodoList() {
   return (
-    <h1>Ola Mundo</h1>
+    <div>
+      <h1>Lista de Tarefas</h1>
+      <form>
+        <input type="text" placeholder="Adicione uma tarefa"/>
+        <button className='add' type="submit">Add</button>
+      </form> 
+      <div className="listaTarefas">
+        <div className="item">
+          <span>Tarefa de exemplo</span>
+          <button>Deletar</button>
+        </div>
+        <div className="itemCompleto">
+          <span>Tarefa de exemplo</span>
+          <button>Deletar</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
